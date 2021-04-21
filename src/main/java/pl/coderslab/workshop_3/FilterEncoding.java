a7d8a9051bf73390389258ef014e20d291d1ca38
ztoczko@wp.pl <Morozow83>
2021-04-21 21:18:30 +0200

    załatanie luki z zabezpieczeniach, courtesy of Ilona Ptak

--- src/main/java/pl/coderslab/workshop_3/FilterEncoding.java
+++ src/main/java/pl/coderslab/workshop_3/FilterEncoding.java
@@ -3,6 +3,7 @@ package pl.coderslab.workshop_3;
 import javax.servlet.*;
 import javax.servlet.annotation.*;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @WebFilter(filterName = "FilterEncoding", value = "/*")
@@ -17,6 +18,9 @@ public class FilterEncoding implements Filter {
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws ServletException, IOException {
         request.setCharacterEncoding("UTF-8");
         response.setCharacterEncoding("UTF-8");
+        ((HttpServletResponse) response).setHeader("Cache-Control", "no-cache, no-store, must-revalidate"); // HTTP 1.1.
+        ((HttpServletResponse) response).setHeader("Pragma", "no-cache"); // HTTP 1.0.
+        ((HttpServletResponse) response).setDateHeader("Expires", 0); // Proxies.
 
         if (!((HttpServletRequest) request).getRequestURI().matches(".+\\..{2,4}")) {
             response.setContentType("text/html");
